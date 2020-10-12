f0ce91f55e6cb4dc36047c097f928b8c65f1136e
Jordon Ogonoski <jordonogonoski@gmail.com>
2020-10-12 16:54:18 -0400

    re-adding everything but with index in root folder

---
+++ js/plugins/PluginTemplate.js
@@ -0,0 +1,32 @@
+//=============================================================================
+// [PLUGIN NAME].js
+//=============================================================================
+var Imported = Imported || {};
+//Imported.[PLUGIN NAME] = true;
+var BMM = BMM || {};
+//BMM.[PLUGIN ABBREVIATION] = BMM.[PLUGIN ABBREVIATION] || {};
+
+/*:
+ * @plugindesc [DESCRIPTION]
+ *
+ * @author NavrasK
+ * 
+ * [parameters]
+ *
+ * @help [INSTRUCTIONS]
+ * TERMS OF USE: No reuse without express permission of author
+ * COMPATIBILITY: No known issues
+ */
+
+(function() {
+	var parameters = PluginManager.parameters("[PLUGIN NAME]");
+
+	//var alias_[PLUGIN NAME]_SceneStart = Scene_Map.prototype.start;
+	//Scene_Map.prototype.start = function() {
+	//	if (!Imported.[REQUIRED PLUGIN NAME]) { // Copy for each plugin that is required
+	//		throw new Error("BMM_[PLUGIN NAME] requires [REQUIRED PLUGIN NAME] to be added to plugin manager");
+	//	}
+	//	alias_[PLUGIN NAME]_SceneStart.call(this);
+	//}
+	// [PLUGIN CODE]
+})();
